529ac1ecb045c659f912a6f9505cc17f4fe1a635
Hynek Zemanec <zemanech@yahoo.com>
2019-06-02 17:40:31 +0200

    feature to create topic and store in database

--- app/forms/newTopic.php
+++ app/forms/newTopic.php
@@ -1,17 +1,22 @@
 <h2 class="header">Create a new topic!</h2>
-<form class="form">
+<?php 
+if (isset($_SESSION['errorMessage'])) {
+    echo '<div class="alert alert-danger" role="alert"><h3 class="subheader">Dang! 😢</h3>' . $_SESSION['errorMessage'] . '</div>';
+    unset($_SESSION['errorMessage']);
+}
+?>
+<form class="form" action="./controllers/topicCreator.php" method="post">
     <div class="form-group">
         <label for="topicName">Name</label>
-        <input type="text" class="form-control" id="topicName" aria-describedby="Topic name" placeholder="Enter the name of new topic...">
-        <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+        <input type="text" class="form-control" name="topicName" id="topicName" aria-describedby="Topic name" placeholder="Enter the name of new topic..." required>
     </div>
     <div class="form-group">
-        <label for="topicDescription">Topic description</label>
-        <textarea class="form-control" id="topicDescription" rows="3" placeholder="Briefly introduce the topic to your audience ..."></textarea>
+        <label for="topicDescription">Topic description (500 chracters max)</label>
+        <textarea class="form-control" name="topicDescription" id="topicDescription" rows="3" placeholder="Briefly introduce the topic to your audience ..." required></textarea>
     </div>
     <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Accessible for registered users only</label>
+        <input type="checkbox" class="form-check-input" name="isPublic" id="isPublic">
+        <label class="form-check-label" for="isPublic" value="1">Accessible for registered users only?</label>
     </div>
     <button type="submit" class="btn btn-primary">Create</button>
 </form>
